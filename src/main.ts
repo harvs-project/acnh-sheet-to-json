7ec1d2e873711e892b580d91e773d8badbb3e298
André <abs@emancipe.se>
2021-03-14 10:06:37 -0300

    linting, code style checks, etc

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,30 @@
-function main() {
-  const filename = "acnh.json";
+export default function main(): void {
+  const filename = 'acnh.json'
   const sheets = SpreadsheetApp.getActiveSpreadsheet()
     .getSheets()
-    .filter((sheet) => sheet.getName() !== "Read Me")
-    .map((sheet) => {
-      const name = sheet.getName();
-      const columns = sheet.getLastColumn() - 1;
-      const rows = sheet.getLastRow() - 1;
+    .filter(sheet => sheet.getName() !== 'Read Me')
+    .map(sheet => {
+      const name = sheet.getName()
+      const columns = sheet.getLastColumn() - 1
+      const rows = sheet.getLastRow() - 1
       const header =
-        sheet.getRange(1, 1, 1, columns).getDisplayValues().pop() || [];
+        sheet.getRange(1, 1, 1, columns).getDisplayValues().pop() || []
       const values =
-        sheet.getRange(2, 1, rows, columns).getDisplayValues() || [];
+        sheet.getRange(2, 1, rows, columns).getDisplayValues() || []
 
       return {
         type: name,
-        items: values.map((cells) =>
+        items: values.map(cells =>
           cells.reduce(
             (acc, cell, column) => ({
               ...acc,
-              ...{ [header[column] as string]: cell },
+              ...{ [header[column] as string]: cell }
             }),
             {}
           )
-        ),
-      };
-    });
+        )
+      }
+    })
 
-  DriveApp.createFile(filename, JSON.stringify(sheets));
+  DriveApp.createFile(filename, JSON.stringify(sheets))
 }
